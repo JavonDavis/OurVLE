54785699fa110877f8be37aedb100bb16cdc51b5
Javon Davis <javonldavis14@gmail.com>
2016-08-03 23:30:19 -0500

    changed courses,forums,profile to tableview

--- OurVLE/CourseViewController.swift
+++ OurVLE/CourseViewController.swift
@@ -9,7 +9,7 @@
 import Foundation
 import UIKit
 
-class CourseViewController: UIViewController {
+class CourseViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
